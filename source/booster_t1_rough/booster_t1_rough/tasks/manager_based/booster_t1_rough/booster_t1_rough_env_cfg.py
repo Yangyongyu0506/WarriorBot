43cac8311eec3bbe3084a03e9e24a5d52a0612d9
yang-yy24 <yang-yy24@mails.tsinghua.edu.cn>
2026-01-30 15:23:53 +0800

    no vel_y

--- source/booster_t1_rough/booster_t1_rough/tasks/manager_based/booster_t1_rough/booster_t1_rough_env_cfg.py
+++ source/booster_t1_rough/booster_t1_rough/tasks/manager_based/booster_t1_rough/booster_t1_rough_env_cfg.py
@@ -394,8 +394,8 @@ class BoosterT1RoughEnvCfg(LocomotionVelocityRoughEnvCfg):
             },
         }
         # Commands (NO SIDEWAYS WALKING INITIALLY)
-        self.commands.base_velocity.ranges.lin_vel_x = (-1.0, 1.0)
-        self.commands.base_velocity.ranges.lin_vel_y = (-1.0, 1.0)
+        self.commands.base_velocity.ranges.lin_vel_x = (-0.2, 1.0)
+        self.commands.base_velocity.ranges.lin_vel_y = (0, 0)
         self.commands.base_velocity.ranges.ang_vel_z = (-1.0, 1.0)
         # Termination tuning
         self.terminations.base_contact.params["sensor_cfg"].body_names = "H2"
